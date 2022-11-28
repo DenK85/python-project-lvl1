1eb65c70dd8bcf42e89382ae1b2d8e062e82a1ff
Denis Korepov <dk777-07@mail.ru>
2022-11-28 15:48:55 +0300

    Add game: parity check

---
+++ brain_games/logic.py
@@ -0,0 +1,23 @@
+import prompt
+from brain_games.even_game import get_answer
+
+
+def logic():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    for round in range(0, 3):
+        correct_answer, question = get_answer()
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+
+        if correct_answer == answer:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
