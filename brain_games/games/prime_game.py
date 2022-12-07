90be016bd1048775b5e4b5f56421390553a8f8d5
Denis Korepov <dk777-07@mail.ru>
2022-12-07 11:31:54 +0300

    Added new game - prime number

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+DESCRIPTION = 'Answer "yes" if number is prime. Otherwise answer "no".'
+LOWER_BOUND = 1
+UPPER_BOUND = 50
+
+
+def get_data():
+    number = randint(LOWER_BOUND, UPPER_BOUND)
+
+    if is_prime(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    question = number
+    return question, correct_answer
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for divider in range(2, number // 2 + 1):
+        if number % divider == 0:
+            return False
+    else:
+        return True
